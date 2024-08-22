dab4a790c7ea0e7f4ba9612763c0e43111afdf5e
pedro <pedroand@hotmail.com>
2024-08-22 09:28:55 +0100

    Users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,6 +68,51 @@ function App() {
       <Footer />
     </HashRouter>
     // </BrowserRouter>
+=======
+import React, { useContext } from 'react';
+import { Route, Routes, HashRouter, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutPage from './components/About';
+import LoginPage from './components/loginComponents/LoginPage';
+import RecoverPass from './components/loginComponents/RecoverPass';
+import RecoverProcess from './components/loginComponents/RecoverProcess';
+import UserManagementPage from './components/pilotComponents/UserManagementPage';
+import Pilots from './components/Pilots';
+import Crew from './components/Crew';
+import Flights from './components/Flights';
+import Master from './components/Master';
+import { AuthContext } from './Context';
+
+function App() {
+  const { token, removeToken, setToken } = useContext(AuthContext);
+
+  return (
+<HashRouter>
+  <Header />
+  <Routes>
+    {!token ? (
+      <>
+        <Route path="/" element={<LoginPage setToken={setToken} removeToken={removeToken} />} />
+        <Route path="/recover" element={<RecoverPass />} />
+        <Route path="/recovery/:token/:email" element={<RecoverProcess />} />
+        <Route path="/about" element={<AboutPage />} />
+      </>
+    ) : (
+      <Route path="/" element={<Master />}>
+        <Route index element={<Navigate replace to="/flights" />} />
+        <Route path="/flights" element={<Flights />} />
+        <Route path="/pilots" element={<Pilots />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/users" element={<UserManagementPage />} />
+      </Route>
+    )}
+    <Route path="*" element={<Navigate replace to="/" />} />
+  </Routes>
+  <Footer />
+</HashRouter>
+
+>>>>>>> Stashed changes
   );
 }
 
